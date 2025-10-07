76120ca6d0fcddef026483652f050b6aa7f67402
Daniele Tomasi <danieletomasi@MACBOOKPRO-B50F.station>
2025-10-07 12:55:17 +0200

    refactor: set MetodoProPage as main and remove old LandingPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
-import LandingPage from './pages/LandingPage';
+import MetodoProPage from './pages/MetodoProPage';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LandingPage />
+  <MetodoProPage />
   </React.StrictMode>
 );
